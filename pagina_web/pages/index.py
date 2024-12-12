bbbf347d4967cb75959606f3ea216c3bf4c0dafb
mmjafet <l202123653@jilotepec.tecnm.mx>
2024-12-11 22:35:31 -0600

    actualizacipn

--- pagina_web/pages/index.py
+++ pagina_web/pages/index.py
@@ -4,8 +4,8 @@ def render():
     return rx.center(
         rx.vstack(
             rx.image(src="/profile_photo.png", width="200px", border_radius="full"),
-            rx.heading("¡Hola, soy Suseth Sandoval! 🚀", size="lg"),
-            rx.text("Soy desarrolladora con experiencia en Flask, optimización y aplicaciones modernas."),
+            rx.heading("Jafet Martinez Meza! 🚀", size="lg"),
+            rx.text("Desarrollador web y estudiante de ingeniería en computación"),    
             rx.button("Ver Proyectos", link="/projects", color_scheme="blue"),
             rx.button("Contacto", link="/contact", color_scheme="green"),
             spacing="5",
